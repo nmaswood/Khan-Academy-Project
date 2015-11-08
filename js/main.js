26785932879461de7cec7447bb50152deff27a32
Nasr Maswood <nasrmaswood@gmail.com>
2015-11-08 17:18:52 -0600

    NEED TO START ERROR TESTING

--- js/main.js
+++ js/main.js
@@ -4,163 +4,44 @@ var editor = ace.edit("editor");
 editor.setTheme("ace/theme/crimson_editor");
 editor.session.setMode("ace/mode/javascript");
 
-var syntaxTree = {};
 
-var domValues = {};
+var domValues = {
+    "whiteList": [],
+    "blackList": [],
+    "structureList": [],
+    "ifToFor": 0,
+    "forToIf": 0,
+    "errorMessage": "",
+    "successMessage": "",
+    "structureMessage" :""
+    };
 
 var domFunctions = {};
 
-syntaxTree.errorMessage = "";
-
-syntaxTree.createSyntaxObject = function() {
-
-    var obj = {
-        "VariableDeclaration": 0,
-        "ForStatement": 0,
-        "IfStatement": 0,
-        "WhileStatement": 0
-    };
-    return obj;
-};
-
-syntaxTree.mySyntaxObject = syntaxTree.createSyntaxObject();
-
-
-
-syntaxTree.createSyntaxTree = function(str){
-
-    if (str === "") {
-        syntaxTree.errorMessage = "Dear User, it seems that the input is empty";
-        return;
-    }
-    try {
-        return esprima.parse(str);
-    }
-    catch(err) {
-        syntaxTree.errorMessage = "Dear User, it seems that your code is unable to be run";
-        console.log(err);
-    }
-};
-
-var A = [];
-/*
-syntaxTree.traverseSyntaxTree = function (node, node_child) {
-    var that = this;
-    var theType = node.type;
-    if (theType in syntaxTree.mySyntaxObject){
-        syntaxTree.mySyntaxObject[theType]++;
-    }
-    for (var category in node) {
-        if (category in node) {
-
-            var leafNode = node[category];
-            if (typeof leafNode === 'object' && leafNode) {
-
-                if (leafNode.constructor === Array ) {
-                    leafNode.forEach(function(node) {
-                        that.traverseSyntaxTree(node);
-                    });
-                } else {
-                    that.traverseSyntaxTree(leafNode);
-                }
-            }
+//IE 8 doesn't support indexOf
+domFunctions.deleteElement = function (element, list) {
+    for (var i = 0; i < list.length; i++) {
+        if (element === list[i]) {
+            list.splice(i, 1);
+            return 1;
         }
     }
-};
-*/
-var IF = "IfStatement";
-var FOR = "ForStatement";
-
-var BOOL = [];
-
-syntaxTree.test = function(){
-    alert("hellow!");
+    return 0;
 };
 
-syntaxTree.traverseSyntaxTree = function (node,obj) {
-    var that = this;
-    if(!node){
-        return;
-    }
-    var theType = node.type;
-    if(obj) {
-        if (theType in obj) {
-            obj[theType]++;
-        }
-    }
-    for (var category in node) {
-
-        if (category in node) {
-
-            var leafNode = node[category];
-            if (typeof leafNode === 'object' && leafNode) {
-
-                if (leafNode.constructor === Array ) {
-                    leafNode.forEach(function(node) {
-                        that.traverseSyntaxTree(node,obj);
-                    });
-                } else {
-                    that.traverseSyntaxTree(leafNode,obj);
-                }
-            }
-        }
-    }
-};
-
-
-var works = 0;
-syntaxTree.find = function(node, outer, inner){
-
-    var that = this;
-
-    if(node["type"] == outer){
-        console.log(node);
-        var one = syntaxTree.createSyntaxObject();
-        syntaxTree.traverseSyntaxTree(node["body"], one);
-
-        if(one[inner]){
-            works++;
-            console.log(works);
+domFunctions.inList = function (element, list) {
+    for (var i = 0; i < list.length; i++) {
+        if (element === list[i]) {
             return true;
         }
     }
-
-    if(node["type"] == outer && "consequent" in node){
-        console.log(node);
-        var one = syntaxTree.createSyntaxObject();
-        syntaxTree.traverseSyntaxTree(node["consequent"], one);
-
-        if(one[inner]){
-            works++;
-            console.log(works);
-            return true;
-        }
-    }
-
-
-    for (var category in node) {
-
-        if (category in node) {
-
-            var leafNode = node[category];
-            if (typeof leafNode === 'object' && leafNode) {
-
-                if (leafNode.constructor === Array ) {
-                    leafNode.forEach(function(node) {
-                        that.find(node, outer, inner);
-                    });
-                } else {
-                    that.find(leafNode, outer, inner);
-                }
-            }
-        }
-    }
+    return false;
 };
 
-domFunctions.insert = function(str, num) {
+domFunctions.insert = function (str, num) {
 
     if (num === 0) {
-        if (!domFunctions.deleteElement(str, domValues.whiteList)){
+        if (!domFunctions.deleteElement(str, domValues.whiteList)) {
             domValues.whiteList.push(str);
         }
         console.log(domValues.whiteList);
@@ -172,12 +53,139 @@ domFunctions.insert = function(str, num) {
     } else {
         if (!domFunctions.deleteElement(str, domValues.structureList)) {
             domValues.structureList.push(str);
-            console.log(domValues.structureList);
         }
     }
 };
 
-domFunctions.run = function(){
+var syntaxTree = {};
+
+syntaxTree.createSyntaxObject = function () {
+    return {
+        "VariableDeclaration": 0,
+        "ForStatement": 0,
+        "IfStatement": 0,
+        "WhileStatement": 0
+    };
+};
+
+syntaxTree.createSyntaxTree = function (str) {
+
+    if (str === "") {
+        domValues.errorMessage = "Dear User, it seems that the input is empty";
+        return;
+    }
+    try {
+        return esprima.parse(str);
+    }
+    catch (err) {
+        domValues.errorMessage = "Dear User, it seems that your code is unable to be run";
+        console.log(err);
+    }
+};
+
+
+syntaxTree.traverseSyntaxTree = function (node, obj) {
+    var that = this;
+    if (!node)
+        return;
+
+    if (obj) {
+        if (node.type in obj) {
+            obj[node.type]++;
+        }
+    }
+    for (var category in node) {
+
+        if (category in node) {
+
+            var leafNode = node[category];
+
+            if (typeof leafNode === 'object' && leafNode) {
+
+                if (leafNode.constructor === Array) {
+                    leafNode.forEach(function (node) {
+                        that.traverseSyntaxTree(node, obj);
+                    });
+                } else {
+                    that.traverseSyntaxTree(leafNode, obj);
+                }
+            }
+        }
+    }
+};
+
+syntaxTree.convertTreeObjToList = function (obj) {
+    var returnList = [];
+
+    for (var key in obj) {
+        if (obj[key]) {
+            returnList.push(key);
+        }
+    }
+
+    return returnList;
+};
+
+
+syntaxTree.find = function (node, outer, inner) {
+
+    var that = this;
+
+    if (node["type"] == outer) {
+
+        var syntaxObject = syntaxTree.createSyntaxObject();
+        syntaxTree.traverseSyntaxTree(node["body"], syntaxObject);
+
+        if (syntaxObject[inner]) {
+            if (inner == "IfStatement") {
+                domValues.forToIf = 1;
+            } else {
+                console.log("SHIT");
+                domValues.ifToFor = 1;
+            }
+        }
+
+    }
+
+    if (node["type"] == outer && "consequent" in node) {
+
+        var syntaxObject = syntaxTree.createSyntaxObject();
+        syntaxTree.traverseSyntaxTree(node["consequent"], syntaxObject);
+
+        if (syntaxObject[inner]) {
+            if (inner == "IfStatement") {
+                domValues.forToIf = 1;
+            } else {
+                console.log("SHIT");
+                domValues.ifToFor = 1;
+            }
+        }
+    }
+
+    for (var category in node) {
+
+        if (category in node) {
+
+            var leafNode = node[category];
+            if (typeof leafNode === 'object' && leafNode) {
+
+                if (leafNode.constructor === Array) {
+                    leafNode.forEach(function (node) {
+                        that.find(node, outer, inner);
+                    });
+                } else {
+                    that.find(leafNode, outer, inner);
+                }
+            }
+        }
+    }
+};
+
+
+domFunctions.run = function () {
+    domValues.errorMessage = "";
+    domValues.successMessage = "";
+    domValues.successMessage = "";
 
     var editorString = editor.getValue();
 
@@ -189,57 +197,90 @@ domFunctions.run = function(){
 
     var listTreeObject = syntaxTree.convertTreeObjToList(emptyTreeObject);
 
-    console.log(listTreeObject);
-    console.log(domValues.whiteList);
-
-    for(var i = 0 ; i < domValues.whiteList.length; i++) {
+    for (var i = 0; i < domValues.whiteList.length; i++) {
 
         if (!domFunctions.inList(domValues.whiteList[i], listTreeObject)) {
-            alert("white list violation");
+            domValues.errorMessage += "  White List Violation";
+            console.log(domValues.errorMessage);
+
         }
     }
-    for(var i = 0 ; i < domValues.blackList.length; i++) {
+
+    if (domValues.whiteList.length && !domValues.errorMessage) {
+        domValues.successMessage += "  White List Success";
+    }
+
+    for (var i = 0; i < domValues.blackList.length; i++) {
 
         if (domFunctions.inList(domValues.blackList[i], listTreeObject)) {
-            alert("black list violation");
+            domValues.errorMessage += "  Black List Violation";
         }
     }
+
+    if (domValues.blackList.length && !domValues.errorMessage) {
+        domValues.successMessage += "  Black List Success";
+
+    }
+    
+    var IF = "IfStatement";
+    var FOR = "ForStatement";
+
+    syntaxTree.find(parsedString, FOR, IF);
+    syntaxTree.find(parsedString, IF, FOR);
+
+    var x = domValues.ifToFor;
+    var y = domValues.forToIf;
+    var z = domValues.structureList.length;
+
+
+    if(z){
+
+        if(z == 2 && x && y){
+            domValues.structureMessage = "FOR then IF and If then FOR Present";
+        } else if(z == 2 && x){
+            domValues.structureMessage = "IF then FOR Present and FOR then IF not present";
+        }
+        else if(z == 2 && y){
+            domValues.structureMessage = "FOR then IF present and FOR then IF not present";
+        } else if(y && domValues.structureList[0] == 'for-to-if'){
+            domValues.structureMessage = "FOR then IF Present";
+        } else if(x && domValues.structureList[0] == 'if-to-for'){
+            domValues.structureMessage = "IF then For Present";
+        }else{
+            domValues.structureMessage = "Structure Not Present";
+        }
+
+    }
+
+
+
+
+    if (domValues.errorMessage) {
+        document.getElementById("error").innerHTML = domValues.errorMessage;
+    }
+    if (domValues.successMessage) {
+        document.getElementById("success").innerHTML = domValues.successMessage;
+    }
+    if(domValues.structureMessage){
+        document.getElementById("structure").innerHTML = domValues.structureMessage;
+    }
+
+    domValues.ifToFor = 0;
+    domValues.forToIf = 0;
+
+    setTimeout(function () {
+            document.getElementById("error").innerHTML = "";
+            document.getElementById("success").innerHTML = "";
+            document.getElementById("structure").innerHTML ="";
+            domValues.errorMessage = "";
+            domValues.successMessage = "";
+            domValues.successMessage = "";
+        },
+        3000);
+
 
 };
 
 
-syntaxTree.convertTreeObjToList = function(obj) {
-    var returnList = [];
-    for (var key in obj) {
-        if (obj[key]){
-            returnList.push(key);
-        }
-    }
-
-    return returnList;
-};
-
-domValues.whiteList = [];
-domValues.blackList = [];
-domValues.structureList = [];
 
 
-//IE 8 doesn't support indexOf
-domFunctions.deleteElement = function(element, list){
-    for(var i = 0; i < list.length; i++){
-        if(element === list[i]){
-            list.splice(i,1);
-            return 1;
-        }
-    }
-    return 0;
-};
-
-domFunctions.inList = function(element, list){
-    for(var i = 0; i < list.length; i++){
-        if(element === list[i]){
-            return true;
-        }
-    }
-    return false;
-};
